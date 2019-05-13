0f6aa9bf94a7b5545b2b2255a421e2f35a074f42
claudisabela <pg35669@alunos.uminho.pt>
2019-05-13 17:23:48 +0100

    initial commit

---
+++ src/main/java/pt/uminho/ceb/biosystems/merlin/merlin_exporter/BackupDatabase.java
@@ -0,0 +1,425 @@
+package pt.uminho.ceb.biosystems.merlin.merlin_exporter;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import es.uvigo.ei.aibench.core.operation.annotation.Direction;
+import es.uvigo.ei.aibench.core.operation.annotation.Operation;
+import es.uvigo.ei.aibench.core.operation.annotation.Port;
+import es.uvigo.ei.aibench.workbench.Workbench;
+
+import pt.uminho.ceb.biosystems.merlin.core.datatypes.Project;
+import pt.uminho.ceb.biosystems.merlin.core.utilities.LoadFromConf;
+import pt.uminho.ceb.biosystems.merlin.utilities.io.FileUtils;
+
+
+/**
+ * @author claudia
+ *
+ */
+@Operation(name="export workspace", description="Make a workspace backup.")
+public class BackupDatabase {
+	
+	
+	private String databaseName;
+	private String initialPath;
+	private String name = "";
+	private Project project = null;
+	private File directory;
+	private long taxID;
+	private Boolean validCredentials = true;
+	private Map<String, String> credentials = LoadFromConf.loadDatabaseCredentials(FileUtils.getConfFolderPath());
+	final static Logger logger = LoggerFactory.getLogger(BackupDatabase.class);
+
+	@Port(direction=Direction.INPUT, name="Select Workspace", validateMethod = "checkProject", order=1)
+	public void setProject(Project project) {
+	
+	}
+
+	/**
+	 * @param project
+	 */
+	public void checkProject(Project project) {
+
+		if(project==null) {
+
+			throw new IllegalArgumentException("Please select a workspace.");
+		}
+		else {
+			this.databaseName = project.getDatabase().getDatabaseName();
+			this.taxID = project.getTaxonomyID();
+			this.project = project;
+
+		} 
+	}
+
+	/**
+	 * @param directory
+	 * @throws IOException 
+	 */
+	@Port(direction=Direction.INPUT, name="Directory:",description="folder",validateMethod="checkDirectory",order=3)
+	public void selectDirectory(File directory){
+
+		try {
+			backupWorkspaceFolder();
+			
+			if (credentials.get("dbtype").equals("mysql")) {
+				backupsqltosql();
+//				backupsqltoh2();
+			}
+			else 
+				backupdbH2();
+			
+			if (validCredentials) {
+				zipBackupFiles();
+				Workbench.getInstance().info("Workspace successfully exported as " + name + ".");
+				logger.info("Workspace successfully exported as " + name);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new IllegalArgumentException("Error while exporting.");
+		}
+
+	}
+	
+
+	private void zipBackupFiles() throws IOException {
+		
+		logger.info("Creating the .mer to export...");
+
+		String path = this.directory.getPath();
+		String extension = ".mer";
+		LocalDateTime currentTime = LocalDateTime.now();
+        String date = "_" + currentTime.getHour() + "h" + currentTime.getMinute() + "m" + currentTime.getSecond() + "s"
+                + currentTime.getDayOfMonth() + currentTime.getMonthValue() + currentTime.getYear();
+		name = "backup_ws_" + databaseName + date + extension;
+
+		if(FileUtils.existsPath(path)){
+
+			FileUtils.createZipFile(path, this.initialPath + "/" + name, 1);
+
+		}
+		else
+			throw new IllegalArgumentException("Error while exporting.");
+		
+		org.apache.commons.io.FileUtils.deleteDirectory(this.directory);
+	}
+
+	private void backupWorkspaceFolder() throws IOException {
+		
+		logger.info("Copying workspace folder files...");
+		String path;
+		String destiny;
+		
+		if (validateDefaultCredentials()) {
+			
+			path = FileUtils.getWorkspaceFolderPath(databaseName);
+			destiny = this.directory+"/"+databaseName;
+			new File(this.directory+"/"+databaseName).mkdirs();
+			
+			File p = new File(path);
+			File d = new File(destiny);
+			
+			org.apache.commons.io.FileUtils.copyDirectory(p, d);
+			
+		}
+		
+		else
+			org.apache.commons.io.FileUtils.deleteDirectory(this.directory);
+			return;
+	}
+
+	/**
+	 * @param directory
+	 */
+	
+	public void checkDirectory(File directory) {
+
+		if(directory == null || directory.toString().isEmpty()) {
+
+			throw new IllegalArgumentException("Please select a directory!");
+		}
+		else {
+			
+			LocalDateTime currentTime = LocalDateTime.now();
+			String date = "_" + currentTime.getHour() + "h" + currentTime.getMinute() + "m" + currentTime.getSecond() + "s"
+					+ currentTime.getDayOfMonth() + currentTime.getMonthValue() + currentTime.getYear();
+
+			if(directory.isDirectory()) {
+				String path = directory.getPath();
+				this.initialPath = path;
+				
+				
+				String newPath = path+"/backup_ws" + date;
+				File newDir = new File(newPath);
+				this.directory = newDir;
+			}
+			else {
+				throw new IllegalArgumentException("Please select a directory!");
+			}
+
+		}
+	}
+	
+	public void backupsqltosql() throws IOException {
+
+		try {
+			
+			logger.info("Starting the SQL database backup...");
+			
+			if (validateDefaultCredentials()) {
+				
+				String username = null, password = null, host = null, port = null, database = null;
+				
+				username = credentials.get("username");
+				password = credentials.get("password");
+				host = credentials.get("host");
+				port = credentials.get("port");
+				
+				
+				database = project.getDatabase().getDatabaseName();
+				
+				dump(host, port, database, username, password, new ArrayList<>());
+				
+			} else {
+				Workbench.getInstance().error("Please configure your MySQL credentials first!");
+				org.apache.commons.io.FileUtils.deleteDirectory(this.directory);
+				
+			}
+				
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+			org.apache.commons.io.FileUtils.deleteDirectory(this.directory);
+		}
+
+	}
+	
+	   
+	public void dump(String host, String port, String database, String username, String password, List<String> tables) throws IOException { 
+
+		try {
+
+			String folderPath = this.directory.getPath();	
+
+			File f1 = new File(folderPath);
+			f1.mkdir();
+
+			String savePath = folderPath + "/backup.sql";
+			
+			String aux = "";
+			
+//			BACKUP SQL TO SQL 
+			String homeFolderPath = FileUtils.getHomeFolderPath().concat("utilities").concat("/");
+			String executeCmd = homeFolderPath + "mysqldump.exe --add-drop-table --user="
+					+ username + " --password=" + password + " --host=" + host + " --port=" + port + " " + database
+					+ " " + aux + " -r " + savePath;
+			
+			Process runtimeProcess = Runtime.getRuntime().exec(executeCmd);
+			int processComplete = runtimeProcess.waitFor();
+
+			
+			if (processComplete == 0) {
+				System.out.println("Backup Complete");
+				
+			} 
+			else {
+				System.out.println("Backup Failure");
+				Workbench.getInstance().error("Error while exporting");
+				org.apache.commons.io.FileUtils.deleteDirectory(this.directory);
+				return;
+			}
+
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+			Workbench.getInstance().error("Error while exporting");
+			org.apache.commons.io.FileUtils.deleteDirectory(this.directory);
+			return;
+			
+		}
+
+	}
+	
+	public void backupdbH2() throws IOException {
+		File homeFile = FileUtils.getHomeFolder();
+		String path = homeFile.getPath();
+		String src;
+		String dest;
+		File source;
+		File destiny;
+		
+		
+		List<String> extensions = new ArrayList<>();
+		extensions.add(".mv.db");
+		extensions.add(".trace.db");
+		
+		
+		try {
+			logger.info("Starting the H2 database backup...");
+			
+			for(String extension : extensions){
+				src = path+"/h2Database/"+this.databaseName + extension;
+				dest = this.directory+"/"+this.databaseName + extension;
+				
+				source = new File(src);
+				destiny = new File(dest);
+				
+				if (source.exists()) {
+					org.apache.commons.io.FileUtils.copyFile(source,destiny);
+				} else {
+					continue;
+				}
+			}
+			
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+			Workbench.getInstance().error("Error while exporting");
+			org.apache.commons.io.FileUtils.deleteDirectory(this.directory);
+			return;
+			
+		}
+		
+		
+	}
+
+//	public void backupsqltoh2() throws IOException {
+//		
+//		try {
+//			
+//			logger.info("Starting the SQL to H2 backup...");
+//
+//			String username = null, password = null, database = null, host = null, port = null, aux = null;
+//
+//			username = credentials.get("username");
+//			password = credentials.get("password");
+//			host = credentials.get("host");
+//			port = credentials.get("port");
+//			aux = "";
+//
+//			database = project.getDatabase().getDatabaseName();
+//
+//			String folderPath = this.directory.getPath();	
+//
+//			File f1 = new File(folderPath);
+//			f1.mkdir();
+//
+//			String savePath = folderPath + "/backupH2.sql";
+//
+//			String executeCmd = "mysqldump --compatible=ansi,no_table_options,no_field_options,no_key_options --hex-blob --skip-opt --user="
+//					+ username + " --password=" + password + " --host=" + host + " --port=" + port + " " + database
+//					+ " " + aux + " -r " + savePath;
+//			
+//			System.out.println(executeCmd);
+//
+//			Process runtimeProcess = Runtime.getRuntime().exec(executeCmd);
+//			int processComplete = runtimeProcess.waitFor();
+//
+//			if (processComplete == 0) {
+//				System.out.println("Backup Complete");
+//			} 
+//			else {
+//				System.out.println("Backup Failure");
+//				org.apache.commons.io.FileUtils.deleteDirectory(this.directory);
+//			}
+//			
+//			List<String> tables = new ArrayList<String>(); //tables is an empty list
+//
+//			rewriteFile(savePath, tables);  
+//			
+//			File saveFile = new File(savePath);
+//			saveFile.delete();
+//		} 
+//		catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//			org.apache.commons.io.FileUtils.deleteDirectory(this.directory);
+//
+//		}
+//
+//	}
+	
+//	public static void rewriteFile(String path, List<String> tables) throws IOException {
+//
+//		File file = new File(path);
+//
+//		BufferedWriter writer = new BufferedWriter(new FileWriter(file.getParent().concat("/").concat("backupSQL_H2.sql"))); 
+//
+//		writer.write("DROP VIEW IF EXISTS `reactions_view_noPath_or_noEC`;");
+//		writer.newLine();
+//		writer.write("DROP TABLE IF EXISTS `reactions_view_noPath_or_noEC`;");
+//		writer.newLine();
+//		writer.write("DROP VIEW IF EXISTS `reactions_view`;");
+//		writer.newLine();
+//		writer.write("DROP TABLE IF EXISTS `reactions_view`;");
+//		writer.newLine();
+//
+//		for(String table : tables) {
+//			writer.write("DROP TABLE IF EXISTS `" + table + "`;");
+//			writer.newLine();
+//		}
+//
+//		writer.newLine();
+//
+//		BufferedReader br = new BufferedReader(new FileReader(file)); 
+//
+//		String st; 
+//		while ((st = br.readLine()) != null) {
+//			writer.write(st);
+//			writer.newLine();
+//		} 
+//
+//		writer.newLine();
+//
+//		writer.write("CREATE  OR REPLACE VIEW reactions_view_noPath_or_noEC AS\r\n" + 
+//				"SELECT DISTINCT idreaction, reaction.name AS reaction_name , equation, reversible, pathway.idpathway, pathway.name AS pathway_name , inModel, isGeneric, reaction.source, originalReaction, compartment_idcompartment , notes\r\n" + 
+//				"FROM reaction\r\n" + 
+//				"LEFT JOIN pathway_has_reaction ON idreaction=pathway_has_reaction.reaction_idreaction\r\n" + 
+//				"LEFT JOIN pathway ON pathway.idpathway=pathway_has_reaction.pathway_idpathway\r\n" + 
+//				"WHERE (idpathway IS NULL ) \r\n" + 
+//				"ORDER BY pathway.name,  reaction.name;");
+//
+//		writer.newLine();
+//
+//		writer.write("CREATE  OR REPLACE VIEW reactions_view AS\r\n" + 
+//				"SELECT DISTINCT idreaction, reaction.name AS reaction_name, equation, reversible, pathway.idpathway, pathway.name AS pathway_name, reaction.inModel, isGeneric, reaction.source, originalReaction, compartment_idcompartment, notes\r\n" + 
+//				"FROM reaction\r\n" + 
+//				"INNER JOIN pathway_has_reaction ON idreaction=pathway_has_reaction.reaction_idreaction\r\n" + 
+//				"INNER JOIN pathway ON pathway.idpathway=pathway_has_reaction.pathway_idpathway\r\n" + 
+//				"WHERE pathway_has_reaction.pathway_idpathway=pathway.idpathway\r\n" + 
+//				"ORDER BY pathway.name, reaction.name;");
+//
+//		writer.close();
+//		br.close();
+//
+//	}
+	
+	private Boolean validateDefaultCredentials() {
+		String username = null, password = null, host = null, port = null;
+		
+		username = credentials.get("username");
+		password = credentials.get("password");
+		host = credentials.get("host");
+		port = credentials.get("port");
+		
+		validCredentials = !(username.equals("your_username") && password.equals("your_password") && host.equals("your_ip_address") && port.equals("3306"));
+		
+		return validCredentials;	
+		
+	}
+
+}
+	
---
+++ src/main/java/pt/uminho/ceb/biosystems/merlin/merlin_exporter/RestoreDatabase.java
@@ -0,0 +1,422 @@
+package pt.uminho.ceb.biosystems.merlin.merlin_exporter;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import es.uvigo.ei.aibench.core.operation.annotation.Direction;
+import es.uvigo.ei.aibench.core.operation.annotation.Operation;
+import es.uvigo.ei.aibench.core.operation.annotation.Port;
+import es.uvigo.ei.aibench.workbench.Workbench;
+
+import pt.uminho.ceb.biosystems.merlin.core.datatypes.Project;
+import pt.uminho.ceb.biosystems.merlin.core.utilities.LoadFromConf;
+import pt.uminho.ceb.biosystems.merlin.core.utilities.MerlinUtils;
+import pt.uminho.ceb.biosystems.merlin.core.utilities.Update;
+import pt.uminho.ceb.biosystems.merlin.database.connector.datatypes.Connection;
+import pt.uminho.ceb.biosystems.merlin.database.connector.datatypes.DatabaseAccess;
+import pt.uminho.ceb.biosystems.merlin.database.connector.datatypes.DatabaseSchemas;
+import pt.uminho.ceb.biosystems.merlin.database.connector.datatypes.Enumerators.DatabaseType;
+import pt.uminho.ceb.biosystems.merlin.utilities.io.FileUtils;
+
+/**
+ * @author claudia
+ *
+ */
+
+@Operation(name="import workspace", description="Import the workspace backup.")
+public class RestoreDatabase {
+	private String databaseType;
+	private String zipPath;
+	private String exportDbType;
+	private String fileName;
+	private String unzippedPath;
+	private String fileDate;
+	private String workspaceName;
+	private String importWS;
+	private Boolean success = true;
+	private String taxId;
+	private Project project;
+	private String destWorkspaceName;
+	private Connection connection;
+	final static Logger logger = LoggerFactory.getLogger(RestoreDatabase.class);
+	
+	@Port(name="workspace:",description="select workspace",direction=Direction.INPUT,order=1)
+	public void setProject(Project project){
+		this.project = project;
+		
+		try {
+			this.connection = new Connection(project.getDatabase().getDatabaseAccess());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+       
+		destWorkspaceName = this.project.getName();
+	}
+	
+
+	/**
+	 * @param directory
+	 * @throws IOException 
+	 */
+	@Port(direction=Direction.INPUT, name="Directory:",description=".mer folder",validateMethod="checkDirectory",order=3)
+	public void selectDirectory(File directory) throws IOException{
+		try {
+			Map<String, String> credentials = LoadFromConf.loadDatabaseCredentials(FileUtils.getConfFolderPath());
+			this.databaseType = credentials.get("dbtype");
+			unzippedPath = FileUtils.getHomeFolderPath().concat("ws").concat("/").concat(destWorkspaceName).concat("/");
+			
+			unzippedPath = unzippedPath + "backup_ws_"+fileDate;
+			
+			logger.info("Starting the .mer folder unzip..." );
+			FileUtils.extractZipFile(zipPath, unzippedPath);
+
+			checkExportDbType();
+			importWorkspaceFolder();
+			
+			switch(this.databaseType) {
+			
+				case "h2":
+					if(exportDbType.equals("H2")) {
+						//H2 to H2
+						importH2ToH2();
+					}else {
+						//MySQL to H2
+						Workbench.getInstance().error("Impossible to import from MySQL to H2 database.");
+						
+					}
+					break;
+					
+				case "mysql":
+					if(exportDbType.equals("MySQL")) {
+						//MySQL to MySQL
+						if (validateDefaultCredentials() )
+							importMySQLToMySQL();
+						else {
+							Workbench.getInstance().error("Please configure your MySQL credentials first!");
+							success = false;
+							
+						}
+							
+					}else {
+						//H2 to MySQL
+						Workbench.getInstance().error("Impossible to import from H2 to MySQL database.");
+						success = false;
+					}
+					break;
+			}
+			
+			File folderDelete = new File(unzippedPath);
+			org.apache.commons.io.FileUtils.deleteDirectory(folderDelete);
+			
+		} catch (Exception e) {
+			success = false;
+			
+			File folderDelete = new File(unzippedPath);
+			org.apache.commons.io.FileUtils.deleteDirectory(folderDelete);
+			
+			e.printStackTrace();
+			throw new IllegalArgumentException("Error while importing.");
+		}
+
+		if(success)
+			Workbench.getInstance().info("Workspace "  + workspaceName + " successfully imported into workspace" + destWorkspaceName + ".");
+			logger.info("Workspace " + workspaceName + " successfully imported.");
+
+	}
+
+//	private void importMySQLToH2() throws Exception {
+//		Map<String, String> credentials = LoadFromConf.loadDatabaseCredentials(FileUtils.getConfFolderPath());
+//		String username = null, password = null, host = null, port = null;
+//		
+//		DatabaseType dbType = DatabaseType.H2;
+//		if (credentials.get("dbtype").equals("mysql"))
+//			dbType = DatabaseType.MYSQL;
+//		
+//		username = credentials.get("username");
+//		password = credentials.get("password");
+//		if (dbType.equals(DatabaseType.MYSQL)) {
+//			host = credentials.get("host");
+//			port = credentials.get("port");
+//		}
+//		
+//		long startTime = System.currentTimeMillis();
+//		
+//		DatabaseSchemas schemas = new DatabaseSchemas(username, password, host, port, dbType);
+//
+//		String filePath=importWS+"backupSQL_H2.sql";
+//		
+//		try {
+//			logger.info("Starting to import from (MySQL) " + workspaceName + " to (H2) " + destWorkspaceName + " database...");
+//			long taxonomyID = Long.parseLong(taxId);
+//
+//			DatabaseAccess dbAccess = this.project.getDatabase().getDatabaseAccess();
+//			
+//			Connection connection = new Connection(dbAccess);
+//			Statement statement = connection.createStatement();
+//			
+//			statement.executeUpdate("DROP ALL OBJECTS");
+//
+//			if(schemas.cleanSchema(destWorkspaceName, filePath)) {
+//				
+//				MerlinUtils.updateAllViews(destWorkspaceName);
+//
+//				Update.checkForUpdates(destWorkspaceName, taxonomyID, false ,statement);
+//
+//			}
+//			else {				
+//				Workbench.getInstance().error("There was an error when trying to import "+ destWorkspaceName +"!!");
+//				File folderDelete = new File(unzippedPath);
+//				org.apache.commons.io.FileUtils.deleteDirectory(folderDelete);
+//				success = false;
+//			}
+//		} catch (Exception e) {
+//			Workbench.getInstance().error("There was an error when trying to import "+ destWorkspaceName +"!!");
+//			e.printStackTrace();
+//			File folderDelete = new File(unzippedPath);
+//			org.apache.commons.io.FileUtils.deleteDirectory(folderDelete);
+//			success = false;
+//			
+//		}
+//	}
+
+	private void importMySQLToMySQL() throws Exception {
+		
+		Map<String, String> credentials = LoadFromConf.loadDatabaseCredentials(FileUtils.getConfFolderPath());
+		String username = null, password = null, host = null, port = null;
+		
+		DatabaseType dbType = DatabaseType.H2;
+		if (credentials.get("dbtype").equals("mysql"))
+			dbType = DatabaseType.MYSQL;
+		
+		username = credentials.get("username");
+		password = credentials.get("password");
+		if (dbType.equals(DatabaseType.MYSQL)) {
+			host = credentials.get("host");
+			port = credentials.get("port");
+		}
+		
+		long startTime = System.currentTimeMillis();
+		
+		DatabaseSchemas schemas = new DatabaseSchemas(username, password, host, port, dbType);
+
+		String filePath=importWS+"backup.sql";
+		
+		try {
+			logger.info("Starting the MySQL workspace " + workspaceName + " import...");
+			
+			long taxonomyID = Long.parseLong(taxId);
+
+			DatabaseAccess dbAccess = this.project.getDatabase().getDatabaseAccess(); 
+			
+			Connection connection = new Connection(dbAccess);
+			
+			Statement statement = connection.createStatement();
+			
+
+			if(schemas.cleanSchema(destWorkspaceName, filePath)) {
+				
+				MerlinUtils.updateAllViews(destWorkspaceName);
+
+				Update.checkForUpdates(destWorkspaceName, taxonomyID, false ,statement);
+			}
+			else {				
+				Workbench.getInstance().error("There was an error when trying to format "+ destWorkspaceName +"!!");
+				File folderDelete = new File(unzippedPath);
+				org.apache.commons.io.FileUtils.deleteDirectory(folderDelete);
+				success = false;
+			}
+		} catch (Exception e) {
+			Workbench.getInstance().error("There was an error when trying to format "+ destWorkspaceName +"!!");
+			e.printStackTrace();
+			File folderDelete = new File(unzippedPath);
+			org.apache.commons.io.FileUtils.deleteDirectory(folderDelete);
+			success = false;
+		}
+	}
+
+	private void importH2ToH2() throws IOException {
+		
+		try {
+			
+			logger.info("Starting the H2 workspace " + workspaceName + " import...");
+			
+			connection.createStatement().execute("SHUTDOWN");  //to delete the .lock file
+			connection.closeConnection();
+
+			String mv = importWS.concat(workspaceName).concat(".mv").concat(".db");
+			String trace = importWS.concat(workspaceName).concat(".trace").concat(".db");
+			File fileMv = new File(mv);
+			File fileTrace = new File(trace);
+			String destinationMv = FileUtils.getHomeFolderPath().concat("h2Database").concat("/").concat(destWorkspaceName).concat(".mv").concat(".db");
+			String destinationTrace = FileUtils.getHomeFolderPath().concat("h2Database").concat("/").concat(destWorkspaceName).concat(".trace").concat(".db");
+			File destMv = new File(destinationMv);
+			File destTrace = new File(destinationTrace);
+						
+			if(fileMv != null) {
+				org.apache.commons.io.FileUtils.copyFile(fileMv, destMv);
+			}
+			
+			if(fileTrace != null) {
+				org.apache.commons.io.FileUtils.copyFile(fileTrace, destTrace);
+			}
+				
+			
+		}
+		catch(Exception e) {
+			System.out.println("Error while importing H2 to H2");
+			Workbench.getInstance().warn("Error while importing H2 files!!");
+			File folderDelete = new File(unzippedPath);
+			org.apache.commons.io.FileUtils.deleteDirectory(folderDelete);
+			success = false;
+		}
+		
+	}
+
+	/**
+	 * @param directory
+	 */
+	
+	public void checkDirectory(File directory) {
+
+		if(directory == null || directory.toString().isEmpty()) {
+			success = false;
+			throw new IllegalArgumentException("Please select a .mer file!");
+		}
+		else {
+			try {
+				zipPath = directory.getPath();
+				fileName = directory.getName();
+				
+				LocalDateTime currentTime = LocalDateTime.now();
+		        
+		        fileDate = currentTime.getHour() + "h" + currentTime.getMinute() + "m" + currentTime.getSecond() + "s"
+		                + currentTime.getDayOfMonth() + currentTime.getMonthValue() + currentTime.getYear();
+				String extension;
+
+				extension = FileUtils.getFileExtension(directory);
+				if(extension.equals(null) || !extension.equals("mer")){
+					success=false;
+					throw new IllegalArgumentException("Please select a .mer file!");
+				}
+
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				success=false;
+				e.printStackTrace();
+				throw new IllegalArgumentException("Please select a .mer file!");
+			}
+
+		}
+	}
+	
+	public void checkExportDbType() throws IOException {
+		logger.info("Getting exported database type...");
+		List <String> foldercontent = FileUtils.getFilesFromFolder(unzippedPath, false);
+		String folder = foldercontent.get(0);
+		importWS = unzippedPath.concat("/").concat(folder).concat("/");
+		List <String> finalFolder = FileUtils.getFilesFromFolder(importWS, false);
+		
+		
+		String path = zipPath;
+		String[] finalPath = path.split(".mer");
+		path = finalPath[0];
+		exportDbType = "";
+		
+		try {
+			for (String fileName : finalFolder) {
+				String dbType = FileUtils.getFileExtension(fileName);
+				if (dbType.equals("sql")) {
+					exportDbType = "MySQL";
+					break;
+				}	
+				else if (dbType.equals("db")){
+					exportDbType = "H2";
+					break;
+				}
+
+			}
+			
+			if(exportDbType.equals("")){
+				success = false;
+				Workbench.getInstance().error("Files not found in selected directory!");
+			}
+			
+		} catch (Exception e) {
+			success = false;
+			File folderDelete = new File(unzippedPath);
+			org.apache.commons.io.FileUtils.deleteDirectory(folderDelete);
+			e.printStackTrace();
+			Workbench.getInstance().error("Files not found in selected directory!");
+
+		}
+
+	}
+	
+	private void importWorkspaceFolder() throws IOException {
+		List <String> foldercontent = FileUtils.getFilesFromFolder(unzippedPath, false);
+		String folder = foldercontent.get(0);
+		importWS = unzippedPath.concat("/").concat(folder).concat("/");
+		List <String> finalFolder = FileUtils.getFilesFromFolder(importWS, false);
+		workspaceName = "";
+		for (String file : finalFolder) {
+			String extension = FileUtils.getFileExtension(file);
+			if(extension.equals("")) { //If the extension is empty, it means that it is a folder. Since there is only one folder, it is the workspace folder.
+				workspaceName = file;
+				break;
+			}	
+		}
+		
+		if(workspaceName.equals("")) {
+			success = false;
+			File folderDelete = new File(unzippedPath);
+			org.apache.commons.io.FileUtils.deleteDirectory(folderDelete);
+			Workbench.getInstance().error("Workspace folder not found");
+		}
+		
+		try {
+			logger.info("Starting the ws folder files import...");
+			String cpy = importWS.concat(workspaceName).concat("/");
+			File copy = new File(cpy);
+			String pst = FileUtils.getHomeFolderPath().concat("ws").concat("/").concat(destWorkspaceName).concat("/");
+			File paste = new File(pst);
+			
+			List<String> taxIdFolder = FileUtils.getFilesFromFolder(importWS.concat(workspaceName), false);
+			taxId = taxIdFolder.get(0);
+			
+			org.apache.commons.io.FileUtils.copyDirectory(copy, paste);
+			
+		} catch (Exception e) {
+			success = false;
+			File folderDelete = new File(unzippedPath);
+			org.apache.commons.io.FileUtils.deleteDirectory(folderDelete);
+		}
+		
+	}
+	
+	private Boolean validateDefaultCredentials() {
+		String username = null, password = null, host = null, port = null;
+		
+		Map<String, String> credentials = LoadFromConf.loadDatabaseCredentials(FileUtils.getConfFolderPath());
+		
+		username = credentials.get("username");
+		password = credentials.get("password");
+		host = credentials.get("host");
+		port = credentials.get("port");
+		
+		Boolean validCredentials = !(username.equals("your_username") && password.equals("your_password") && host.equals("your_ip_address") && port.equals("3306"));
+		
+		return validCredentials;
+		
+	}
+	
+}
